53df0db3e7d22a240581bd7aac79db955fdda185
NiteshKamble <kamblenitesh351@gmail.com>
2023-04-09 23:19:09 +0530

    basic animation redering done.

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,30 @@
+import { useRef } from "react";
+import DisplaySection from "./componets/DisplaySection";
+import Jumbotron from "./componets/Jumbotron";
+import Nav from "./componets/Nav";
+import SoundSection from "./componets/SoundSection";
+import WebgiViewer from "./componets/WebgiViewer";
 
 function App() {
 
+  const webGiViewerRef = useRef();
+  const contentref = useRef();
+
+  const handlerPreView = () => {
+
+    webGiViewerRef.current.triggerPreview();
+
+  }
+
   return (
     <div className="App">
-      Hello
+      <div id="content" ref={contentref}>
+        <Nav />
+        <Jumbotron />
+        <SoundSection />
+        <DisplaySection triggerPreview={handlerPreView} />
+      </div>
+      <WebgiViewer contentref={contentref}  ref={webGiViewerRef} />
     </div>
   );
 }
